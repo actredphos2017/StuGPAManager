76054be564380f91301c4f9a75e88e86d05ace67
Sakunoakarinn <actredphos2017@gmail.com>
2022-12-11 19:48:18 +0800

    SQL Connect Support

--- Report.h
+++ Report.h
@@ -9,23 +9,37 @@
 #include <vector>
 #include <iostream>
 
+#include "cppconn/driver.h"
+#include "cppconn/connection.h"
+#include "cppconn/statement.h"
+#include "cppconn/prepared_statement.h"
+
 #include "Student.h"
 
 using namespace std;
+using namespace sql;
 
 class Report {
 private:
-    list<Student*> stus;
+    bool connectSQL = false;
+    Driver*     dri = nullptr;
+    Connection* con = nullptr;
+
+    list<Student*> container;
+
 public:
     void insert(Student* stu);
     void insert(Report& re);
+
     void remove(Report& re);
     void remove(const string& condition);
+
     Report& operator[](const string& condition);
     friend ostream& operator<<(ostream& os, Report& re);
 
     bool read(istream& is, ostream& errorOs = cout);
     bool write(ostream& os, ostream& errorOs = cout);
+
     friend istream& operator>>(istream& os, Report& re);
 };
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+
+#define TEST_CPPCONN false
+
+#if !TEST_CPPCONN
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+#endif
+
+
+
+//test jdbc
+#if TEST_CPPCONN
+
+#include "cppconn/driver.h"
+#include "cppconn/connection.h"
+
+int main(){
+    sql::Driver* dri = get_driver_instance();
+    sql::Connection* con = dri->connect("tcp://127.0.0.1","root","gamemode1");
+}
+
+#endif
